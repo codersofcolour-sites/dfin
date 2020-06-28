1e7fc7fee1b34f799e09210f9504a4a6a1a74e0d
Tomisin-O <onasanyatomisin@gmail.com>
2020-06-29 00:49:35 +0100

    banner on flex pages

--- flex/models.py
+++ flex/models.py
@@ -1,9 +1,10 @@
 """Flexible page."""
 from django.db import models
 
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
-from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, PageChooserPanel
+from wagtail.core.fields import StreamField, RichTextField
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 from streams import blocks 
 # Create your models here.
@@ -14,6 +15,22 @@ class FlexPage(Page):
 
     template = "flex/flex_page.html"
 
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    banner_subtitle = RichTextField(features=["bold", "italic"])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+   )
     content = StreamField(
         [
             ("title_and_text", blocks.TitleAndTextBlock()),
@@ -29,6 +46,10 @@ class FlexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
         StreamFieldPanel("content"),
+        FieldPanel("banner_title"),
+        ImageChooserPanel("banner_image"),
+        PageChooserPanel("banner_cta")
+
     ]
 
     class Meta:  # noqa 
